9c35eb8f857f37d7bcc2546d1444d83456d0cc47
Herve <herve.mathonet>
2017-02-02 15:02:44 +0100

    .

--- src/app/items/item.service.ts
+++ src/app/items/item.service.ts
@@ -1,9 +1,7 @@
 import { Item } from './item';
-import { ITEMS } from './mock-items';
 
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-//import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
